1889994aad74f9bf1d164a1019919f73489ae071
“anjumol” <anjumol.sadmani@gmail.com>
2025-10-20 21:34:07 +0200

    feat: Implement circuit breaker pattern for vendor service calls

--- services/order-service/build.gradle.kts
+++ services/order-service/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     id("io.spring.dependency-management") version "1.1.4"
 }
 
+val resilience4jVersion = "2.3.0"
+
 
 dependencies{
     implementation("org.springframework.boot:spring-boot-starter-web")
@@ -11,5 +13,8 @@ dependencies{
 
     compileOnly("org.projectlombok:lombok:1.18.42")
     annotationProcessor("org.projectlombok:lombok:1.18.42")
+
+    implementation("io.github.resilience4j:resilience4j-spring-boot3:${resilience4jVersion}")
+    implementation("io.github.resilience4j:resilience4j-circuitbreaker:${resilience4jVersion}")
 
 }
---
+++ services/order-service/src/main/java/com/nibble/orderservice/service/VendorClient.java
@@ -0,0 +1,27 @@
+package com.nibble.orderservice.service;
+
+import com.nibble.orderservice.dto.ItemDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+
+@Service
+public class VendorClient {
+
+    @CircuitBreaker(name = "vendorService", fallbackMethod = "checkStockAvailabilityFallback")
+    public boolean checkStockAvailability(Long vendorId, List<ItemDto> items){
+        if (Math.random() > 0.5) {
+            System.out.println("Vendor service failed!");
+            throw new RuntimeException("Vendor service is down!");
+        }
+        System.out.println("Vendor service responded successfully");
+        return true;
+    }
+
+    private boolean checkStockAvailabilityFallback(Long vendorId, List<ItemDto> items, Exception e){
+        System.out.println("Circuit breaker fallback triggered: " + e.getMessage());
+        return false;
+    }
+}
