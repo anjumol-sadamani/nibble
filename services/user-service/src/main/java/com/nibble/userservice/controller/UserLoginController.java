2bbda85fac9a171fcd9d81bd1f072a23a1180d53
Athira Sabu <athirasudhaks@gmail.com>
2026-01-03 23:38:33 +0200

    refactor:review comments update

--- services/user-service/src/main/java/com/nibble/userservice/controller/UserLoginController.java
+++ services/user-service/src/main/java/com/nibble/userservice/controller/UserLoginController.java
@@ -22,7 +22,15 @@ public class UserLoginController {
 
         String email = user.getEmail();
         String username = user.getGivenName();
-        return ResponseEntity.ok(userService.saveOrUpdate(email, username));
+        String message;
+        if (userService.isExistingUser(email, username)) {
+            message = "Welcome back, " + username + "!";
+        } else {
+            userService.save(email, username);
+            message = "Welcome to Nibble, " + username + "!";
+        }
+
+        return ResponseEntity.ok(message);
     }
 
 }
--- services/user-service/src/main/java/com/nibble/userservice/service/UserServiceImpl.java
+++ services/user-service/src/main/java/com/nibble/userservice/service/UserServiceImpl.java
@@ -16,23 +16,13 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
 
     @Override
-    public String saveOrUpdate(String email, String username) {
-        String message;
-        if (isExistingUser(email, username)) {
-            message = "Welcome back, " + username + "!";
-        } else {
-            save(email, username);
-            message = "Welcome to Nibble, " + username + "!";
-        }
-        return message;
-    }
-
-    private void save(String email, String username) {
+    public void save(String email, String username) {
         final User newUser = new User(email, username);
         userRepository.save(newUser);
     }
 
-    private boolean isExistingUser(String email, String username) {
+    @Override
+    public boolean isExistingUser(String email, String username) {
         final Optional<User> existingUser = userRepository.findByEmail(email);
         return existingUser.isPresent();
     }
